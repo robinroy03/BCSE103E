60d1e4373bd5773a0ad33bbba8decfb599a941df
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2023-07-31 23:00:11 +0530

    Update printfunction.java

--- printfunction.java
+++ printfunction.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+// A sample demonstration on taking I/O using Scanner class.
+
 public class printfunction{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
