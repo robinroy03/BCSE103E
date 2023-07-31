9bdea48c9a330d075f6f14c980937b125f4b975d
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2023-07-31 22:59:22 +0530

    Add files via upload

---
+++ printfunction.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class printfunction{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String no;
+    no = sc.nextLine();
+    System.out.println(no);
+    sc.close();
+  }
+}
